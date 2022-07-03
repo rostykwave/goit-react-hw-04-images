6ba759acac4275332608ceee1492bf3538a2a0de
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-07-03 16:39:22 +0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Searchbar } from './components/Searchbar/Searchbar';
 import 'react-toastify/dist/ReactToastify.css';
 import { Box } from 'styleConfig/Box';
-import { Component } from 'react';
+// import { Component } from 'react';
 import { fetchImagesAPI } from 'services/pixabay-api';
 import { ImageGallery } from './components/ImageGallery/ImageGallery';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Status = {
   IDLE: 'idle',
@@ -12,76 +14,153 @@ const Status = {
   REJECTED: 'rejected',
 };
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    page: 1,
-    leftPages: 0,
-    images: [],
-    error: { message: '' },
-    status: Status.IDLE,
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [leftPages, setLeftPages] = useState(0);
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState({ message: '' });
+  const [status, setStatus] = useState(Status.IDLE);
+
+  const handleFormSubmit = CurrentSearchQuery => {
+    if (CurrentSearchQuery !== searchQuery) {
+      setSearchQuery(CurrentSearchQuery);
+      setPage(1);
+      setImages([]);
+      // this.setState({ searchQuery, page: 1, images: [] });
+    }
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.searchQuery !== this.state.searchQuery ||
-      prevState.page !== this.state.page
-    ) {
-      this.fetchImages();
-    }
-  }
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
+    // this.setState(state => ({ page: state.page + 1 }));
+  };
 
-  fetchImages = () => {
+  useEffect(() => {
+    // fetchImages();
     const perPage = 12;
-    const { searchQuery, page } = this.state;
-    this.setState({ status: Status.PENDING });
+    console.log('fetch');
 
-    fetchImagesAPI(searchQuery, page, perPage)
-      .then(data => {
-        // console.log(data);
-        const images = data.hits;
+    searchQuery && fetchImages();
 
-        if (images.length === 0) {
-          throw Error('There is no images found on this search result');
-        }
-        const totalHits = data.totalHits;
-        const leftPages = Math.ceil(totalHits / perPage) - page;
+    async function fetchImages() {
+      try {
+        // const { searchQuery, page } = this.state;
+        setStatus(Status.PENDING);
+        // this.setState({ status: Status.PENDING });
+        await fetchImagesAPI(searchQuery, page, perPage).then(data => {
+          // console.log(data);
+          const images = data.hits;
 
-        this.setState(prevState => {
-          return {
-            images: [...prevState.images, ...images],
-            status: Status.RESOLVED,
-            leftPages,
-          };
+          if (images.length === 0) {
+            throw Error('There is no images found on this search result');
+          }
+          const totalHits = data.totalHits;
+          const currentLeftPages = Math.ceil(totalHits / perPage) - page;
+
+          setImages(prevImages => [...prevImages, ...images]);
+          setStatus(Status.RESOLVED);
+          setLeftPages(currentLeftPages);
         });
-      })
-      .catch(error => this.setState({ error, status: Status.REJECTED }));
-  };
 
-  handleFormSubmit = searchQuery => {
-    if (searchQuery !== this.state.searchQuery) {
-      this.setState({ searchQuery, page: 1, images: [] });
+        // this.setState(prevState => {
+        //   return {
+        //     images: [...prevState.images, ...images],
+        //     status: Status.RESOLVED,
+        //     leftPages,
+        //   };
+        // });
+      } catch (error) {
+        setError(error);
+        setStatus(Status.REJECTED);
+        // this.setState({ error, status: Status.REJECTED });
+      }
     }
-  };
+  }, [searchQuery, page]);
 
-  loadMore = () => {
-    this.setState(state => ({ page: state.page + 1 }));
-  };
+  return (
+    <Box display="grid" gridTemplateColumns="1fr" gridGap="16px" pb="24px">
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ImageGallery
+        status={status}
+        images={images}
+        leftPages={leftPages}
+        error={error}
+        loadMore={loadMore}
+      />
+    </Box>
+  );
+};
+// export class App1 extends Component {
+//   state = {
+//     searchQuery: '',
+//     page: 1,
+//     leftPages: 0,
+//     images: [],
+//     error: { message: '' },
+//     status: Status.IDLE,
+//   };
 
-  render() {
-    const { status, images, leftPages, error } = this.state;
+//   componentDidUpdate(prevProps, prevState) {
+//     if (
+//       prevState.searchQuery !== this.state.searchQuery ||
+//       prevState.page !== this.state.page
+//     ) {
+//       this.fetchImages();
+//     }
+//   }
 
-    return (
-      <Box display="grid" gridTemplateColumns="1fr" gridGap="16px" pb="24px">
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery
-          status={status}
-          images={images}
-          leftPages={leftPages}
-          error={error}
-          loadMore={this.loadMore}
-        />
-      </Box>
-    );
-  }
-}
+//   fetchImages = () => {
+//     const perPage = 12;
+//     const { searchQuery, page } = this.state;
+//     this.setState({ status: Status.PENDING });
+
+//     fetchImagesAPI(searchQuery, page, perPage)
+//       .then(data => {
+//         // console.log(data);
+//         const images = data.hits;
+
+//         if (images.length === 0) {
+//           throw Error('There is no images found on this search result');
+//         }
+//         const totalHits = data.totalHits;
+//         const leftPages = Math.ceil(totalHits / perPage) - page;
+
+//         this.setState(prevState => {
+//           return {
+//             images: [...prevState.images, ...images],
+//             status: Status.RESOLVED,
+//             leftPages,
+//           };
+//         });
+//       })
+//       .catch(error => this.setState({ error, status: Status.REJECTED }));
+//   };
+
+//   handleFormSubmit = searchQuery => {
+//     if (searchQuery !== this.state.searchQuery) {
+//       this.setState({ searchQuery, page: 1, images: [] });
+//     }
+//   };
+
+//   loadMore = () => {
+//     this.setState(state => ({ page: state.page + 1 }));
+//   };
+
+//   render() {
+//     const { status, images, leftPages, error } = this.state;
+
+//     return (
+//       <Box display="grid" gridTemplateColumns="1fr" gridGap="16px" pb="24px">
+//         <Searchbar onSubmit={this.handleFormSubmit} />
+//         <ImageGallery
+//           status={status}
+//           images={images}
+//           leftPages={leftPages}
+//           error={error}
+//           loadMore={this.loadMore}
+//         />
+//       </Box>
+//     );
+//   }
+// }
